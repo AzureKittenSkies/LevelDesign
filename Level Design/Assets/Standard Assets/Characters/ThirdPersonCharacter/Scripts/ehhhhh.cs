01facb7b021a741414713f3260202039d691b04b
Unknown <azzhayman@gmail.com>
2018-07-31 15:59:49 +1000

    First push

---
+++ Level Design/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ehhhhh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ehhhhh : MonoBehaviour {
+
+    public Animator anim;
+    public bool isMoving;
+    [Range(-1,1)]
+    public float horizontal, vertical;
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+    }
+}
